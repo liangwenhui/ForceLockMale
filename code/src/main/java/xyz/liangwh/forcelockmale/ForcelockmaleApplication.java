a7c9836c7d6824b3f8ddeeada1b0e62b50f6f42a
liangwh <0027015537@iwhalecloud.com>
2020-06-23 17:19:18 +0800

    create project

---
+++ code/src/main/java/xyz/liangwh/forcelockmale/ForcelockmaleApplication.java
@@ -0,0 +1,13 @@
+package xyz.liangwh.forcelockmale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForcelockmaleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForcelockmaleApplication.class, args);
+    }
+
+}
---
+++ code/src/test/java/xyz/liangwh/forcelockmale/ForcelockmaleApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.liangwh.forcelockmale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForcelockmaleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
